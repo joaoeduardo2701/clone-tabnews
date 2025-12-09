ce89374322aaf9e067fa4400d7f5f29982836cff
Eduardo Cardoso <63940232+joaoeduardo2701@users.noreply.github.com>
2025-12-09 09:42:14 +0000

    Adicona os scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Eu vou conseguir a minha primeira vaga como desenvolvedor em 2026</h1>
+  return (
+    <h1>Eu vou conseguir a minha primeira vaga como desenvolvedor em 2026</h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
