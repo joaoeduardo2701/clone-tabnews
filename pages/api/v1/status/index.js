52ca03923c52ed87319a97e4fa2ae606e9dcd1c3
Eduardo Cardoso <63940232+joaoeduardo2701@users.noreply.github.com>
2025-12-24 00:26:35 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as soma;");
+
+  console.log(result.rows);
+
   response.status(200).json({
     Nome: "João",
   });
