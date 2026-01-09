a687f1ad0ab06046a124e7bd5212b352edc0192b
Eduardo Cardoso <63940232+joaoeduardo2701@users.noreply.github.com>
2026-01-09 02:10:28 +0000

    finish `/api/v1/status` enpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,36 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as soma;");
+  const database = require("infra/database").default;
 
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersion = await database
+    .query("SHOW server_version;")
+    .then((res) => res.rows[0].server_version);
+
+  const databaseMaxConnections = await database
+    .query("SHOW max_connections;")
+    .then((res) => res.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseUsingConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseUsingConnectionsValue =
+    databaseUsingConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    Nome: "João",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        current_connections: databaseUsingConnectionsValue,
+      },
+    },
   });
 }
 
